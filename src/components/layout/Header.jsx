b5b3c98ab1473127ee62c73df4746134463b9cd4
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-07 18:33:27 -0700

    Styles added to the header.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,6 +5,24 @@ import MenuIcon from "@material-ui/core/Menu"
 
 export default class Header extends React.Component {
   render() {
-    return <AppBar position='fixed'>Musical Events</AppBar>
+    return (
+      <AppBar
+        position='fixed'
+        style={{
+          backgroundColor: "#2f2f2f",
+          boxShadow: "none",
+          padding: "10px 0px"
+        }}
+      >
+        <Toolbar>
+          <div className='header_logo'>
+            <div className='font_righteous header_logo_venue'>
+              Musical Events
+            </div>
+            <div className='header_logo_title'>Enjoy The Music</div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    )
   }
 }
