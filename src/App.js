667135d991d6781f2643d97bf108a0269b8d8a72
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-10 19:14:24 -0700

    Added info-section component to the app.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 import "./App.css"
 import Header from "./components/layout/Header"
 import Home from "./components/home"
+import Info from "./components/info-section"
 
 function App() {
   return (
     <div className='App' style={{ height: "1500px", background: "red" }}>
       <Header />
       <Home></Home>
+      <Info></Info>
     </div>
   )
 }
