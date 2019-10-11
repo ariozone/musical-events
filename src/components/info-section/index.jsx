f8418bcf8fe5eab4d1d7640868c3f2a312619e94
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-10 19:34:21 -0700

    Added the static info to the info-section

--- src/components/info-section/index.jsx
+++ src/components/info-section/index.jsx
@@ -16,17 +16,21 @@ const Info = () => {
                   className='vn_icon'
                   style={{ background: `url(${icon_calendar})` }}
                 ></div>
+                <div className='vn_title'>Event Date and Time</div>
+                <div className='vn_desc'>4 January 2020 at 9 pm</div>
               </div>
             </div>
           </div>
           <div className='vn_item'>
             <div className='vn_outer'>
               <div className='vn_inner'>
-                <div className='vn_icon_square bck_red'></div>
+                <div className='vn_icon_square bck_yellow'></div>
                 <div
                   className='vn_icon'
                   style={{ background: `url(${icon_location})` }}
                 ></div>
+                <div className='vn_title'>Event Location</div>
+                <div className='vn_desc'>Segerstrom Center of the Arts</div>
               </div>
             </div>
           </div>
