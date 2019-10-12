f483fbed0e32d3beb4186ca38b85bf4ac6fd22f0
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-11 17:10:00 -0700

    Added transition to info section elements using react-reveal Zoom

--- src/components/info-section/index.jsx
+++ src/components/info-section/index.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import Zoom from "react-reveal/Zoom"
 import icon_calendar from "../../resources/images/icons/calendar.png"
 import icon_location from "../../resources/images/icons/location.png"
 
@@ -8,32 +8,37 @@ const Info = () => {
     <div className='bck_black'>
       <div className='center_wrapper'>
         <div className='vn_wrapper'>
-          <div className='vn_item'>
-            <div className='vn_outer'>
-              <div className='vn_inner'>
-                <div className='vn_icon_square bck_red'></div>
-                <div
-                  className='vn_icon'
-                  style={{ background: `url(${icon_calendar})` }}
-                ></div>
-                <div className='vn_title'>Event Date and Time</div>
-                <div className='vn_desc'>4 January 2020 at 9 pm</div>
+          <Zoom duration={500}>
+            <div className='vn_item'>
+              <div className='vn_outer'>
+                <div className='vn_inner'>
+                  <div className='vn_icon_square bck_red'></div>
+                  <div
+                    className='vn_icon'
+                    style={{ background: `url(${icon_calendar})` }}
+                  ></div>
+                  <div className='vn_title'>Event Date and Time</div>
+                  <div className='vn_desc'>4 January 2020 at 9 pm</div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='vn_item'>
-            <div className='vn_outer'>
-              <div className='vn_inner'>
-                <div className='vn_icon_square bck_yellow'></div>
-                <div
-                  className='vn_icon'
-                  style={{ background: `url(${icon_location})` }}
-                ></div>
-                <div className='vn_title'>Event Location</div>
-                <div className='vn_desc'>Segerstrom Center of the Arts</div>
+          </Zoom>
+
+          <Zoom duration={500} delay={300}>
+            <div className='vn_item'>
+              <div className='vn_outer'>
+                <div className='vn_inner'>
+                  <div className='vn_icon_square bck_yellow'></div>
+                  <div
+                    className='vn_icon'
+                    style={{ background: `url(${icon_location})` }}
+                  ></div>
+                  <div className='vn_title'>Event Location</div>
+                  <div className='vn_desc'>Segerstrom Center of the Arts</div>
+                </div>
               </div>
             </div>
-          </div>
+          </Zoom>
         </div>
       </div>
     </div>
