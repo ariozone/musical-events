0674ca51612f8d25ebf4317d72f9af9a0a2ac5bd
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-14 19:03:26 -0700

    Transition added to discount info section.

--- src/components/info-section/discount.jsx
+++ src/components/info-section/discount.jsx
@@ -1,25 +1,44 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Fade, Slide } from "react-reveal"
+
 const Discount = () => {
+  const [discountStart, setDiscountStart] = useState(0)
+  const [discountEnd] = useState(35)
+
+  const loadPercentage = () => {
+    discountStart < discountEnd && setDiscountStart(discountStart + 1)
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      loadPercentage()
+    }, 50)
+  }, [discountStart])
+
   return (
     <div className='center_wrapper'>
       <div className='discount_wrapper'>
-        <div className='discount_percentage'>
-          <span>35%</span>
-          <span>OFF</span>
-        </div>
-        <div className='discount_description'>
-          <h3>Purchase tickets before Jan 2020</h3>
-          <p>
-            If you’re verified as a Fan Club member, you’ll receive a text prior
-            to the Verified Fan Fan Club Member presale that begins on Monday,
-            October 14th at 10 am local time. The general public Verified Fan
-            presale will follow during this date. Being verified for either
-            presale would mean you’d receive a link to purchase and a unique
-            access code. This text would come anywhere from 2-4 hours prior to
-            the presale at 10 am local time.
-          </p>
-        </div>
+        <Fade onReveal={() => loadPercentage()}>
+          <div className='discount_percentage'>
+            <span>{discountStart}%</span>
+            <span>OFF</span>
+          </div>
+        </Fade>
+        <Slide right>
+          <div className='discount_description'>
+            <h3>Purchase tickets before Jan 2020</h3>
+            <p>
+              If you’re verified as a Fan Club member, you’ll receive a text
+              prior to the Verified Fan Fan Club Member presale that begins on
+              Monday, October 14th at 10 am local time. The general public
+              Verified Fan presale will follow during this date. Being verified
+              for either presale would mean you’d receive a link to purchase and
+              a unique access code. This text would come anywhere from 2-4 hours
+              prior to the presale at 10 am local time.
+            </p>
+            <button>Purchase Now</button>
+          </div>
+        </Slide>
       </div>
     </div>
   )
