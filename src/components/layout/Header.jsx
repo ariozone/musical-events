7a5146507b7c0df27b43dfe7b51985d1f5d8d3b5
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-17 12:05:35 -0700

    Convert Header class component to a functional component using react hooks.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,64 +1,56 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { AppBar, Toolbar, IconButton } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
 import SideDrawer from "./sideDrawer"
 
-export default class Header extends React.Component {
-  state = {
-    open: false,
-    header: false
-  }
+const Header = () => {
+  const [open, setOpen] = React.useState(false)
+  const [header, setHeader] = React.useState(false)
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll)
-  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, [])
 
   // componentWillUnmount() {
   //   window.removeEventListener("scroll")
   // }
 
-  handleScroll = () => {
-    window.scrollY > 0
-      ? this.setState({ header: true })
-      : this.setState({ header: false })
+  const handleScroll = () => {
+    window.scrollY > 0 ? setHeader(true) : setHeader(false)
   }
-  toggleClose = value => {
-    this.setState({ open: value })
+  const toggleClose = value => {
+    setOpen(value)
   }
 
-  render() {
-    return (
-      <AppBar
-        position='fixed'
-        style={{
-          backgroundColor: this.state.header ? "#2f2f2f" : "transparent",
-          boxShadow: "none",
-          padding: "10px 0px"
-        }}
-      >
-        <Toolbar>
-          <div style={{ flexGrow: "1" }}>
-            <div className='font_righteous ' style={{ fontSize: "40px" }}>
-              Musical Events
-            </div>
-            <div className='header_logo_title'>
-              Taylor Swift Lover Fest West at SoFi Stadium
-            </div>
+  return (
+    <AppBar
+      position='fixed'
+      style={{
+        backgroundColor: header ? "#2f2f2f" : "transparent",
+        boxShadow: "none",
+        padding: "10px 0px"
+      }}
+    >
+      <Toolbar>
+        <div style={{ flexGrow: "1" }}>
+          <div className='font_righteous ' style={{ fontSize: "40px" }}>
+            Musical Events
           </div>
+          <div className='header_logo_title'>
+            Taylor Swift Lover Fest West at SoFi Stadium
+          </div>
+        </div>
 
-          <IconButton
-            aria-label='Menu'
-            color='inherit'
-            onClick={() => this.toggleClose(true)}
-          >
-            <MenuIcon></MenuIcon>
-          </IconButton>
-          <SideDrawer
-            open={this.state.open}
-            onClose={() => this.toggleClose(false)}
-          ></SideDrawer>
-        </Toolbar>
-      </AppBar>
-    )
-  }
+        <IconButton
+          aria-label='Menu'
+          color='inherit'
+          onClick={() => toggleClose(true)}
+        >
+          <MenuIcon></MenuIcon>
+        </IconButton>
+        <SideDrawer open={open} onClose={() => toggleClose(false)}></SideDrawer>
+      </Toolbar>
+    </AppBar>
+  )
 }
+export default Header
