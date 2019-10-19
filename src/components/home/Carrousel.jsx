24a22a9eda13e1a30a827511b8f756d0a5c97fbd
Ario Tamaddoni <ariozone6@gmail.com>
2019-10-18 20:35:00 -0700

    Refactored the images to a seperate file.

--- src/components/home/Carrousel.jsx
+++ src/components/home/Carrousel.jsx
@@ -1,18 +1,6 @@
 import React from "react"
 import Slider from "react-slick"
-
-import ts1 from "../../resources/images/ts1.jpg"
-import ts2 from "../../resources/images/ts2.jpg"
-import ts3 from "../../resources/images/ts3.jpg"
-import ts4 from "../../resources/images/ts4.jpg"
-import ts5 from "../../resources/images/ts5.jpg"
-import ts6 from "../../resources/images/ts6.jpg"
-import ts7 from "../../resources/images/ts7.jpg"
-import ts8 from "../../resources/images/ts8.jpg"
-import ts9 from "../../resources/images/ts9.jpg"
-import ts0 from "../../resources/images/ts0.jpg"
-
-export const imagesArr = [ts1, ts2, ts3, ts4, ts5, ts6, ts7, ts8, ts9, ts0]
+import imagesArr from "../../resources/images/images"
 
 const Caurrousel = () => {
   const settings = {
